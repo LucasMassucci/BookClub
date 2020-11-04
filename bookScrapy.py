315c042f0d0f0f13bdb07ca8a1b1a677cdd4e527
Lucas Massucci <lucas.massucci@outlook.com>
2020-11-04 01:21:32 -0300

    rm hotspots + add test + pep8 + rm code smell

--- bookScrapy.py
+++ bookScrapy.py
@@ -2,115 +2,125 @@ import pandas as pd
 
 from pandas.io import sql
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.chrome.options import Options
 from word2number import w2n
 from sqlalchemy import create_engine
-
 
 
 def driver_init():
     option = Options()
     option.headless = True
-    #'/home/lucas-massucci/Projects/DataEngineering/BookClubProjectBasic/chromedriver',
-
+    # '/home/lucas-massucci/Projects/Data-Engineering/BookClub/chromedriver,'
     driver = webdriver.Chrome(options=option)
 
     return driver
 
+
 def engine_init():
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
-    
-    if not engine.dialect.has_schema(engine,'book_club'):
+    engine = create_engine(
+        'postgresql://postgres:postgres@localhost:5432/postgres')
+
+    if not engine.dialect.has_schema(engine, 'book_club'):
         sql.execute('CREATE SCHEMA IF NOT EXISTS book_club', engine)
     else:
         sql.execute('DROP TABLE IF EXISTS book_club.books', engine)
-    
+
     return engine
 
 
 def scrapy_category_lists(driver):
-    driver.get('http://books.toscrape.com/index.html')
+    driver.get('https://books.toscrape.com/index.html')
 
-    web_elements =  driver.find_elements_by_xpath(
-        '//div[@class="side_categories"]//ul[@class="nav nav-list"]//li//ul//li//a')
-    
-    category_links = [web_elements[item].get_attribute("href") 
+    web_elements = driver.find_elements_by_xpath(
+        '//div[@class="side_categories"]//ul[@class="nav nav-list"]'
+        + '//li//ul//li//a')
+
+    category_links = [web_elements[item].get_attribute("href")
+                      for item in range(len(web_elements))]
+
+    category_list = [web_elements[item].text
                      for item in range(len(web_elements))]
 
-    category_list = [web_elements[item].text 
-                     for item in range(len(web_elements))]
-    
     return category_links, category_list
+
 
 def scrapy_rows(driver):
     return driver.find_elements_by_xpath(
         '//li[@class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]'
-        +'//article[@class="product_pod"]')
+        + '//article[@class="product_pod"]')
 
 
-def extract_data(rows:list, category: str):
+def extract_data(rows: list, category: str):
 
-    row_texts = [rows[r].text.split('\n') for r in range(len(rows))]  
+    row_texts = [rows[r].text.split('\n') for r in range(len(rows))]
     titles = rows[0].find_elements_by_xpath('//h3//a')
     ratings_data = rows[0].find_elements_by_xpath('//p')
 
     data = {
-            'title':  [my_title.get_attribute("title") 
-                       for my_title in titles ],
+            'title':  [my_title.get_attribute("title")
+                       for my_title in titles],
 
             'price':  [text[1].replace('£', '') for text in row_texts],
 
             'rating': [w2n.word_to_num(
                        rating.get_attribute('class')
-                       .replace('star-rating ', '')) 
-                       for rating in ratings_data 
+                       .replace('star-rating ', ''))
+                       for rating in ratings_data
                        if rating.get_attribute('class')
                        .__contains__('star-rating')],
 
-            'in_stock':  [True if text[2] == 'In stock' else False 
+            'in_stock':  [True if text[2] == 'In stock' else False
                           for text in row_texts],
 
-            'category': [category for text in row_texts]
+            'category': [category] * len(row_texts)
             }
 
     return data
 
-def to_postgres(data,engine,schema='book_club', table='books'):
-    df = pd.DataFrame (data, columns = ['title','price','rating',
-                                        'in_stock','category'])
-    df.to_sql(table, engine, schema, 
-              if_exists='append',index=False)
+
+def to_postgres(data, engine, schema='book_club', table='books'):
+    df = pd.DataFrame(data, columns=['title', 'price', 'rating',
+                                     'in_stock', 'category'])
+    df.to_sql(table, engine, schema,
+              if_exists='append', index=False)
+
 
 def extract_next_page(driver):
     next_page = driver.find_elements_by_xpath(
         '//div//ul[@class="pager"]//li[@class="next"]//a')
     for np in next_page:
-        return np.get_attribute("href")      
+        return np.get_attribute("href")
+
 
 def sql_image_to_csv(engine):
     pd.DataFrame(
         sql.execute('Select * From book_club.books', engine),
-        columns = ['title','price','rating','in_stock','category']
+        columns=['title', 'price', 'rating', 'in_stock', 'category']
         ).to_csv('out.csv', index=False)
 
 
-def extration_clycle(url, category,engine):
+def extration_clycle(url, category, engine, schema='book_club', table='books'):
     driver.get(url)
     data = extract_data(scrapy_rows(driver), category)
-    to_postgres(data,engine)
+    to_postgres(data, engine)
     return extract_next_page(driver)
 
-def process(driver, engine):    
+
+def process(driver, engine):
     category_links, category_list = scrapy_category_lists(driver)
-    
-    for item in range(len(category_links)):    
-        category = category_list[item] 
+    default_schema = 'book_club'
+    default_table = 'books'
+
+    for item in range(len(category_links)):
+        category = category_list[item]
         url = category_links[item]
-        next_page = extration_clycle(url,category,engine)
-        
+        next_page = extration_clycle(url, category, engine,
+                                     default_schema, default_table)
+
         while next_page:
-            next_page = extration_clycle(next_page, category, engine)
+            next_page = extration_clycle(next_page, category, engine,
+                                         default_schema, default_table)
+
 
 driver = driver_init()
 engine = engine_init()
